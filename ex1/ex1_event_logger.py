09756f7204b57a3aaeac649558975a40d3c4378f
Gandalfthegloom <farrellarifandipurbasidadolog@gmail.com>
2025-01-27 20:50:36 -0500

    Finished ex_1_event_logger.py

--- ex1/ex1_event_logger.py
+++ ex1/ex1_event_logger.py
@@ -48,19 +48,25 @@ class Event:
     # If you want to create a special type of Event for your game that requires a different
     # set of attributes, you can do that separately in the project1 folder. This class is part of
     # Exercise 1 and will be auto-graded.
-
-    # TODO: Add attributes below based on the provided descriptions above. Use the specified datatypes.
+    id_num: int
+    description: str
+    next_command: Optional[str]
+    next: Optional['Event']
+    prev: Optional['Event']
 
 
 class EventList:
     """
     A linked list of game events.
 
+    # TODO add descriptions of instance attributes here
     Instance Attributes:
-        - # TODO add descriptions of instance attributes here
+        - first: points towards (or contains) the first event in the list.
+        - last: points towards (or contains) the last event in the list.
 
+    # TODO add any appropriate representation invariants, if needed
     Representation Invariants:
-        - # TODO add any appropriate representation invariants, if needed
+        - (self.first is None and self.last is None) or (self.first is not None and self.last is not None)
     """
     first: Optional[Event]
     last: Optional[Event]
@@ -78,12 +84,11 @@ class EventList:
             print(f"Location: {curr.id_num}, Command: {curr.next_command}")
             curr = curr.next
 
-    # TODO: Complete the methods below, based on the given descriptions. Do NOT change any of their specification.
     #  That is, the function headers (parameters, return type, etc.) must NOT be changed.
     def is_empty(self) -> bool:
         """Return whether this event list is empty."""
 
-        # TODO: Your code below
+        return self.first is None
 
     def add_event(self, event: Event, command: Optional[str] = None) -> None:
         """Add the given new event to the end of this event list.
@@ -92,7 +97,15 @@ class EventList:
         """
         # Hint: You should update the previous node's <next_command> as needed
 
-        # TODO: Your code below
+        if self.is_empty():
+            self.first = event
+            self.last = event
+            event.prev = None
+        else:
+            self.last.next_command = command
+            self.last.next = event
+            event.prev = self.last
+            self.last = event
 
     def remove_last_event(self) -> None:
         """Remove the last event from this event list.
@@ -100,23 +113,35 @@ class EventList:
 
         # Hint: The <next_command> and <next> attributes for the new last event should be updated as needed
 
-        # TODO: Your code below
+        if isinstance(self.first) is None:
+            return
+        elif self.first == self.last:
+            self.first = None
+            self.last = None
+        else:
+            self.last = self.last.prev
+            self.last.next = None
+            self.last.next_command = None
 
     def get_id_log(self) -> list[int]:
         """Return a list of all location IDs visited for each event in this list, in sequence."""
 
-        # TODO: Your code below
+        event = self.first
+        id_list = []
+        while event is not None:
+            id_list.append(event.id_num)
+            event = event.next
+        return id_list
 
     # Note: You may add other methods to this class as needed but DO NOT CHANGE THE SPECIFICATION OF ANY OF THE ABOVE
 
 
 if __name__ == "__main__":
-    pass
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (Delete the "#" and space before each line.)
     # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
-    # import python_ta
-    # python_ta.check_all(config={
-    #     'max-line-length': 120,
-    #     'disable': ['R1705', 'E9998', 'E9999']
-    # })
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'disable': ['R1705', 'E9998', 'E9999']
+    })
