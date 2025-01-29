87226f673ab1d37ce69e1d953179326db1ea12da
Gandalfthegloom <farrellarifandipurbasidadolog@gmail.com>
2025-01-28 21:21:42 -0500

    Cleaned up whitespaces

--- ex1/ex1_simulation.py
+++ ex1/ex1_simulation.py
@@ -149,7 +149,7 @@ class AdventureGameSimulation:
                 next_command=None,
                 next=None,
                 prev=None
-            )  
+            )
             self._events.add_event(new_event, command)
             current_loc = next_loc
 
@@ -192,8 +192,8 @@ if __name__ == "__main__":
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (Delete the "#" and space before each line.)
     # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
-    import python_ta
-    python_ta.check_all(config={
-        'max-line-length': 120,
-        'disable': ['R1705', 'E9998', 'E9999']
-    })
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120,
+    #     'disable': ['R1705', 'E9998', 'E9999']
+    # })
